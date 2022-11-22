6cc115d0fa22cf9d8e669c18b5d6ddd8ddb990d3
Vladimir <urkytsk3@gmail.com>
2022-11-22 11:12:30 +0800

    полностью реализовал базовую модель InotifyPropertyChanged

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Efir.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Efir.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        private string _Title;
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #region БЛОК МЕДИА
+
+        #region прогресс бар
+        private int _ValueProgressDownlaodingSeries = 0;
+        /// <summary>
+        /// Индикатор загрузки для сериалов
+        /// </summary>
+        public int ValueProgressDownlaodingSeries
+        {
+            get => _ValueProgressDownlaodingSeries;
+            set => Set(ref _ValueProgressDownlaodingSeries, value);
+        }
+        #endregion
+
+        #endregion
+
+
+    }
+}
