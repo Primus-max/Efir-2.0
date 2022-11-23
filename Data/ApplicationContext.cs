06d5940c7ea9f3ae31a1ece9d94ee3a5344a0f71
Vladimir <urkytsk3@gmail.com>
2022-11-23 10:06:27 +0800

    Добавил модель - Коллекция развлекательного(список директорий)

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -20,6 +20,7 @@ namespace Efir.Data
         public DbSet<SeriesCollection> SeriesCollections { get; set; } = null!;
         public DbSet<DocumentariesCollection> DocumentariesCollections { get; set; } = null!;
         public DbSet<EducationalCollection> EducationalCollections { get; set; } = null!;
+        public DbSet<EntertainmentCollection> EntertainmentCollections { get; set; } = null!;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=MediaDB.db");
