753f2a1ccbec6cddfddcdb96cd3ac81cea18e143
Vladimir <urkytsk3@gmail.com>
2022-12-07 06:59:35 +0800

    Добавл метод для воскресения

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2790,9 +2790,10 @@ namespace Efir
                                 int s = substractTimeWithinEvents.Seconds;
 
                                 int totalMinuteEvent = h + m;
-
-                                //------------------------------------------поиск контента------------------------------------------//
                                 int totalMinute = totalMinuteEvent;
+                                //------------------------------------------поиск контента------------------------------------------//
+
+                                #region ФИЛЬМЫ
                                 if (model.EventListSourceSunday[i].EventName == "ФИЛЬМЫ")
                                 {
                                     PrintSunday print = new PrintSunday();
@@ -2840,6 +2841,70 @@ namespace Efir
                                         elseFilm = true;
                                     }
                                 }
+                                #endregion
+
+                                #region СЕРИАЛЫ
+                                //int totalMinute = totalMinuteEvent;
+                                if (model.EventListSourceMonday[i].EventName == "СЕРИАЛЫ")
+                                {
+                                    List<Series> series = context.Serieses.ToList();
+                                    PrintSunday? print = new PrintSunday();
+                                    bool elseFilm = false;
+
+
+                                    int hh = 0;
+                                    int mm = 0;
+
+
+                                    var listSortedByDate = context.Serieses.ToList().OrderBy(s => s.LastRun);//сортирую лист по дате
+                                    Series sortedLastItemByDate = listSortedByDate.Last(); // получаю последнюю просмотренную серию 
+                                    int indexElement = series.IndexOf(sortedLastItemByDate);// узнаю индекс этой серии в листе такого же вида, в котором ищую эту серию
+
+                                IfLengthIsOver:
+                                    for (int j = indexElement; j < listSortedByDate.Count(); j++)
+                                    {
+                                        #region Определение времени
+                                        hh = series[j].Duration.Hours * 60;
+                                        mm = series[j].Duration.Minutes;
+
+                                        int curMinuteEvent = hh + mm;
+                                        #endregion
+
+                                        if (curMinuteEvent > totalMinute) break;
+
+                                        TimeSpan addedTime = TimeSpan.FromMinutes(curMinuteEvent);
+                                        string[] splitName = series[j].Name.Split(".");
+                                        string formattedName = splitName[0];
+
+                                        print.TimeToEfir = !elseFilm ? curItemTime.TimeToEfir : print.TimeToEfir + addedTime;
+                                        print.EventName = formattedName;
+                                        print.Series = series[j].NumOfSeries > 0 ? series[j].IsSeries : 0;
+                                        print.Description = "Сериал: ";
+                                        series[j].LastRun = DateTime.Now;
+
+                                        if (print.TimeToEfir > nextItemTime.TimeToEfir) break;
+
+                                        Guid guid = Guid.NewGuid();
+                                        string RandomId = guid.ToString();
+
+                                        print.Id = RandomId;
+
+                                        context.PrintSundays.Add(print);
+                                        context.SaveChanges();
+
+                                        TheRestTime = totalMinute - curMinuteEvent;
+                                        totalMinute = TheRestTime;
+                                        elseFilm = true;
+
+                                        if (i == series.Count - 1)
+                                        {
+                                            indexElement = 0;
+                                            goto IfLengthIsOver;
+                                        }
+                                    }
+
+                                }
+                                #endregion
                             }
                         }
                     }
